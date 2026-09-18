b0bfb9aa3abede9c4823e4b9d81c744e63ca81c3
Lukáš Typl <typllukas@gmail.com>
2026-09-18 08:32:00 +0200

    deps: add Symfony 7.4, Doctrine, the Elasticsearch client and the quality tooling

---
+++ config/bundles.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['test' => true],
+];
